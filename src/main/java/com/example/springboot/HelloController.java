1f0ba5c7305d0332a38dfa336bbcf2a9e3f853f6
Siarhei Svila <47723898+SiarheiSvila@users.noreply.github.com>
2024-08-13 20:47:39 +0000

    add create operation

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,12 +22,20 @@ public class HelloController {
 		return "Greetings from Spring Boot!";
 	}
 
-	@GetMapping("/query")
+	@GetMapping("/get-all")
 	public List<String> printSecrets() {
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
-
 		return this.jdbcTemplate.queryForList("SELECT * FROM vals").stream()
 				.map((m) -> m.values().toString())
 				.collect(Collectors.toList());
+	}
+
+	@GetMapping("/create")
+	public String printSecrets(@RequestParam String name) {
+		jdbcTemplate.update(
+			"INSERT INTO vals(name) VALUES (?)",
+			name
+		);
+
+		return "OK";
 	}
 }
