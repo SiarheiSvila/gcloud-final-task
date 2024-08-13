46b4b44e0b5ae4560894f6f5d0dc8abdb70a46c3
Siarhei Svila <47723898+SiarheiSvila@users.noreply.github.com>
2024-08-13 18:32:17 +0000

    add import

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import java.util.Arrays;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
