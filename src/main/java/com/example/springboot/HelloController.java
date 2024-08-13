b01a7d52aceb6506c52ffcaae7a72956fc95ca5b
Siarhei Svila <47723898+SiarheiSvila@users.noreply.github.com>
2024-08-13 12:57:01 +0000

    add db access

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,9 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	private final JdbcTemplate jdbcTemplate;
+
+	public HelloController(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 
+	@GetMapping("/query")
+	public List<String> printSecrets() {
+		this.jdbcTemplate.queryForList("SELECT * FROM val").stream()
+				.map((m) -> m.values().toString())
+				.collect(Collectors.toList());
+	}
 }
