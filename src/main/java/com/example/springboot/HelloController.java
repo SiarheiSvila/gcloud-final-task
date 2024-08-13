bff83635607d2bc864e0f475647b49def501ab59
Siarhei Svila <47723898+SiarheiSvila@users.noreply.github.com>
2024-08-13 13:20:15 +0000

    add imports to controller

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,9 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class HelloController {
@@ -19,7 +22,7 @@ public class HelloController {
 
 	@GetMapping("/query")
 	public List<String> printSecrets() {
-		this.jdbcTemplate.queryForList("SELECT * FROM val").stream()
+		return this.jdbcTemplate.queryForList("SELECT * FROM val").stream()
 				.map((m) -> m.values().toString())
 				.collect(Collectors.toList());
 	}
